db02d3fd7784269e235874a8f88742088b1eabe7
Dilip-92 <chechu.sikar@gmail.com>
2026-03-19 16:31:36 +0530

    Create example configuration file

---
+++ config.example.js
@@ -0,0 +1,17 @@
+// Example Configuration Template
+
+module.exports = {
+    // Database configuration
+    database: {
+        host: 'localhost',
+        port: 27017,
+        dbName: 'your_database_name',
+        user: 'your_username',
+        password: 'your_password'
+    },
+    // Server configuration
+    server: {
+        port: 3000,
+    },
+    // Add other configurations here
+};
